2ae7ebdfaa9ae740575e365269ac956903cb25e2
meatball <ethan.sith@gmail.com>
2016-10-10 21:25:02 -0700

    Search in rotated sorted array

---
+++ Java/rotatedSortedArray/rotatedSortedArray.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public int search(int[] A, int target) {
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+        int start = 0, end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[start] < A[mid]) {
+                if (A[start] <= target && target <= A[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            } else {  
+                // A[start] > A[mid]
+                if (A[mid] <= target && target <= A[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+        if (A[start] == target) {
+            return start;
+        } else if (A[end] == target) {
+            return end;
+        } else {
+            return -1;
+        }
+    }
+}
+
