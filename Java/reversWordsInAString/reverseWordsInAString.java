c4d1633bdecbac55a47005b9da9cb7e6c23ef900
meatball <ethan.sith@gmail.com>
2016-10-11 21:01:12 -0700

    Reverse Words In A String

---
+++ Java/reversWordsInAString/reverseWordsInAString.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String reverseWords(String s) {
+        if (s.length() == 0 || s == null) {
+            return "";
+        }
+        String[] splited = s.split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for (int i = splited.length - 1; i >= 0; i--) {
+            if (!splited.equals("")) {
+                result.append(splited[i]).append(" ");
+            }
+        }
+        return result.length() == 0 ? "" : result.substring(0, result.length()- 1);
+    }
+}
