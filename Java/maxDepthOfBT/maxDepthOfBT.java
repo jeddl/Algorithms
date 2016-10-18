c8d4455ac8fb3e321a0806e5074de972f8e55cc4
meatball <ethan.sith@gmail.com>
2016-10-17 20:59:35 -0700

    Add 2 exercises

---
+++ Java/maxDepthOfBT/maxDepthOfBT.java
@@ -0,0 +1,32 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * Definition of TreeNode:
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left, right;
+     *     public TreeNode(int val) {
+     *         this.val = val;
+     *         this.left = this.right = null;
+     *     }
+     * }
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
