709d3fc42c6c02765ed9a7ac9643999c458c3d8e
meatball <ethan.sith@gmail.com>
2016-10-26 21:41:29 -0700

    Add One Exercise
     - LCA

---
+++ Java/LCA/LCA.java
@@ -0,0 +1,31 @@
+public class Solution {
+    /**
+     * Definition of TreeNode:
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left, right;
+     *     public TreeNode(int val) {
+     *         this.val = val;
+     *         this.left = this.right = null;
+     *     }
+     * }
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
+        if (root == null || root == A || root == B) {
+            return root;
+        }
+        
+        TreeNode left = lowestCommonAncestor(root.left, A, B);
+        TreeNode right = lowestCommonAncestor(root.right, A, B);
+        
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        }
+        
+        return null;
+    }
+}
