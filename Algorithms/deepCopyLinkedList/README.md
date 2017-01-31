0b0e59b9f5f8cc15e46ccbbcd6ca2c59886b72c8
meatball <ethan.sith@gmail.com>
2017-01-30 21:40:00 -0800

    Add One Exercise
     - Deep Copy Linked List

---
+++ Algorithms/deepCopyLinkedList/README.md
@@ -0,0 +1,9 @@
+# Deep Copy LinkedList
+## Example
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+## Solution
+- Use a HashMap to track LinkedList nodes and the random pointer for each node
+- Need to create a new LinkList instance
---
+++ Algorithms/deepCopyLinkedList/deepCopyLinkedList.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        RandomListNode dummy = new RandomListNode(0);
+        RandomListNode prev = dummy;
+        RandomListNode newNode = dummy;
+        HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+
+        while (head != null) {
+            if (map.containsKey(head)) {
+                newNode = map.get(head);
+            } else {
+                newNode = new RandomListNode(head.label);
+                map.put(head, newNode);
+            }
+
+            prev.next = newNode;
+
+            if (head.random != null) {
+                if (map.containsKey(head.random)) {
+                    newNode.random = map.get(head.random);
+                } else {
+                    newNode.random = new RandomListNode(head.random.label);
+                    map.put(head.random, newNode.random);
+                }
+            }
+
+            prev = newNode;
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}
